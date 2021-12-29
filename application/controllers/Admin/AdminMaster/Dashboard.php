768e3d9e3c705292b19b18296e40b9b86e3be600
akbaridc <akbarimawan18@gmail.com>
2021-12-29 23:33:45 +0700

    session master admin

--- application/controllers/Admin/AdminMaster/Dashboard.php
+++ application/controllers/Admin/AdminMaster/Dashboard.php
@@ -12,17 +12,21 @@ class Dashboard extends CI_Controller
 
     public function index()
     {
-        $data = [
-            'title' => 'Dashboard - Master Admin',
-            'active' => 'master-admin',
-            'data' => $this->Setting_website_model->getDataWebsite(),
-            'icon' => $this->Setting_website_model->getDataSosmed()->result_array(),
-            'user' => $this->db->get_where('user_tbl')->num_rows(),
-            'member' => $this->db->get_where('member_tbl')->num_rows(),
-            'produk' => $this->db->get_where('produk_tbl')->num_rows(),
-            'reward' => $this->db->get_where('reward_tbl')->num_rows(),
-            'produk_terjual' => $this->db->get_where('member_produk_tbl', ['status' => 2])->num_rows(),
-        ];
-        $this->template2->views('admin/master-admin/dashboard', $data);
+        if ($this->session->userdata('id')) {
+            $data = [
+                'title' => 'Dashboard - Master Admin',
+                'active' => 'master-admin',
+                'data' => $this->Setting_website_model->getDataWebsite(),
+                'icon' => $this->Setting_website_model->getDataSosmed()->result_array(),
+                'user' => $this->db->get_where('user_tbl')->num_rows(),
+                'member' => $this->db->get_where('member_tbl')->num_rows(),
+                'produk' => $this->db->get_where('produk_tbl')->num_rows(),
+                'reward' => $this->db->get_where('reward_tbl')->num_rows(),
+                'produk_terjual' => $this->db->get_where('member_produk_tbl', ['status' => 2])->num_rows(),
+            ];
+            $this->template2->views('admin/master-admin/dashboard', $data);
+        } else {
+            redirect('login/pihak');
+        }
     }
 }
